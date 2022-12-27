7a220d75bc298c7fac65be424c49e5d9692a421c
ivanhack34 <48842215+ivanhack34@users.noreply.github.com>
2022-12-27 10:34:10 -0400

    Parte 1
    Ejercicios de javascript practice del 1 al 3 completos

---
+++ Ejercicios.js
@@ -0,0 +1,30 @@
+//?1-Write a function that takes two numbers (a and b) as argument. Sum a and b. Return the result
+/*
+function myFunction (a, b){
+return a + b
+}
+
+console.log(myFunction(1, 2));
+*/
+
+//?2-Write a function that takes two values, say a and b, as arguments. Return true if the two values are equal and of the same type
+/*
+function myFunction(a, b){
+return a === b
+}
+
+console.log(myFunction(1, 2));
+console.log(myFunction(2, 2));
+*/
+
+//?3-Write a function that takes a value as argument. Return the type of the value.
+/*
+function myFunction(a) {
+    return typeof a;
+ }
+
+ console.log(myFunction(1));
+ console.log(myFunction('hola'));
+ */
+
+ //?4-Write a function that takes a string (a) and a number (n) as argument. Return the nth character of 'a'.
