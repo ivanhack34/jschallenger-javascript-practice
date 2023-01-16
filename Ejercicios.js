9678d219968ae4d2cbb69c9f048bad0455bf3519
ivanhack34 <48842215+ivanhack34@users.noreply.github.com>
2023-01-16 16:32:15 -0400

    Part 2
    Challenge practice 4 and five

--- Ejercicios.js
+++ Ejercicios.js
@@ -27,4 +27,31 @@ function myFunction(a) {
  console.log(myFunction('hola'));
  */
 
+//todo: Challenger parte 2
+
  //?4-Write a function that takes a string (a) and a number (n) as argument. Return the nth character of 'a'.
+/* 
+function myFunction(a, n) {
+    return a[n - 1];
+ }
+
+ console.log(myFunction('abcd',1));
+console.log( myFunction('zyxbwpl',5) );
+console.log(myFunction('gfedcba',3));
+*/
+
+//?5-Write a function that takes a string (a) as argument. Remove the first 3 characters of a. Return the result
+
+/*
+function myFunction(a) {
+    return a.slice(3);
+  }
+
+  console.log(myFunction('abcdefg'));
+  console.log(myFunction('1234'));
+  console.log(myFunction('fgedcba'));
+  */
+
+//?6-Write a function that takes a string as argument. Extract the last 3 characters from the string. Return the result
+
+
